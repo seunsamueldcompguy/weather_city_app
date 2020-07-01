377c97cb95e2ac89cf40c7ae642a0e76915115fd
seunsamueldcompguy <seun.samuel.dcomputer.guy@gmail.com>
2020-07-01 05:31:13 -0930

    models, templates and view

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    city_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.city_name}'
+
+    class Meta:
+        verbose_name_plural = 'cities'
+
--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+import requests
+from .models import City
 
-# Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e4ffd5e7ea455ca0b6ad57d3147d3343'
+    city = 'Abuja'
+
+
+
+    cities = City.objects.all()
+    weather_data = []
+
+    for city in cities:
+        response = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.city_name,
+            'temperature': response['main']['temp'],
+            'description': response['weather'][0]['description'],
+            'icon': response['weather'][0]['icon']
+        }
+
+    weather_data.append(city_weather)
+
+    return render(request, 'weather/home.html', {'weather_data': weather_data})
+
